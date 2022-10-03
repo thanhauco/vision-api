a0c30e6483bdc75d450c2920d5e5219fccb4c2ee
Thanh Vu <thanhauco@gmail.com>
2022-10-03 07:52:00 -0700

    Add ViT Model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,3 @@
-import torchvision.models as models
-def get_model():
-    return models.resnet50(pretrained=True)
+import timm
+def get_vit():
+    return timm.create_model('vit_base_patch16_224', pretrained=True)
